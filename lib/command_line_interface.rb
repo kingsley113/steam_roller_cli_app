fbc53049a53b1fc11d12f4ef551940e4a848c4cf
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-01-16 21:29:02 +0000

    Add Psuedocode to Game, Developer, CLI, Scraper class

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -7,19 +7,27 @@ require "colorize"
 class CommandLineInterface
 # establish attributes
 
-# initialize
+# run
 
 # welcome user, prompt for which list from steam to access
 
 # get user input for list selection
 
 # pass user selection to scraper to get info
+#   make game objects from 'new release' list
+#   make game objects from 'highest rated' list
 
-# diaplay selected list back to user
+
+# display selected list back to user
 
 # promp user for additional action
 
 # take user input and call next method
 
+# return sorted list - aplhabetically
+# return sorted list - price
+# return sorted list - rating
+# return sorted list - by developer alphabetically
+# return sorted list - release date
 
 end
--- lib/game.rb
+++ lib/game.rb
@@ -11,13 +11,10 @@ class Game
 
 # reset all game objects
 
+# create game objects
+
 # check for missing info - pass to scraper to get info
 
-# return sorted list - aplhabetically
-# return sorted list - price
-# return sorted list - rating
-# return sorted list - by developer alphabetically
-# return sorted list - release date
 
 # return info on one specific game instance
 end
