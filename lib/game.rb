94c70d5d252cd78dbad6061d5c5cd0fe8dec5de0
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-01-16 21:44:22 +0000

    Develop methods in Game class

--- lib/game.rb
+++ lib/game.rb
@@ -2,19 +2,38 @@ require 'pry'
 
 class Game
 # establish attributes
+  attr_accessor :name, :price, :release_date, :developer, :rating, :page_link
+
+  @@all = []
 
 # initialize
+  def initialize(name, price = nil) #initializing with only name and price data from first scrape
+    @name = name
+    @price = price
+  end
 
 # save
+  def save
+    @all << self
+  end
 
 # all - class method
+  def self.all?
+    @@all
+  end
 
 # reset all game objects
+  def reset_all
+    @@all.clear
+  end
 
 # create game objects
 
 # check for missing info - pass to scraper to get info
-
+  def add_missing_info
+  # check for empty attributes
+  # call scraper class to get info from specific game page
+  end
 
 # return info on one specific game instance
 end
