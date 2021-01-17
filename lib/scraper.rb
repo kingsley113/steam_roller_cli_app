d8e9b0f19eee2275266cdb0c8403700cc8a07deb
Cameron Kingsley <cameron.kingsley13@gmail.com>
2021-01-17 23:12:58 +0000

    Develop Scraper Class

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,20 +5,25 @@ class Scraper
 # establish attributes
 @@new_release_link = "https://store.steampowered.com/games/#p=0&tab=NewReleases"
 @@top_sellers_link = "https://store.steampowered.com/games/#p=0&tab=TopSellers"
-
+@@all = []
 # scrape Steam new release games page
   def scrape_new_release_page
     # get html data from website
     doc = Nokogiri::HTML(open(@@new_release_link))
     # create array of list of game elements on page
-    all_games = doc.css(".tab_content_ctn.sub, NewReleaseRows")
-    #complete attribute info on each game
-    all_games.each_with_index do |info, index|
-      game_name =
-      binding.pry
+    all_games = doc.css('div#NewReleasesRows a')
+
+    #scrape basic info on each game available from the list page
+    all_games.each do |info|
+      game_link   = info["href"]
+      game_name   = info.css(".tab_item_name").text
+      game_price  = info.css(".discount_final_price").text
+
+      game_info = {:link = game_link, :name = game_name, :price = game_price}
+      @@all << game_info
+      # binding.pry
     end
-    # game_name = first_game.css(".tab_item_name").text
-    # binding.pry
+    binding.pry
   end
 # scrape Steam highest rated games page
 
